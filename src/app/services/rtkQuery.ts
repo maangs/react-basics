73cd430133cca0b17c43e900f566457d671b9428
Johan Magnusson <magnusson1987@gmail.com>
2023-11-09 21:38:54 +0100

    clean up interface

--- src/app/services/rtkQuery.ts
+++ src/app/services/rtkQuery.ts
@@ -5,23 +5,8 @@ export interface user {
   name: string;
   username: string;
   email: string;
-  //   address: {
-  //     street: "Kulas Light";
-  //     suite: "Apt. 556";
-  //     city: "Gwenborough";
-  //     zipcode: "92998-3874";
-  //     geo: {
-  //       lat: "-37.3159";
-  //       lng: "81.1496";
-  //     };
-  //   };
   phone: string;
   website: string;
-  //   company: {
-  //     name: "Romaguera-Crona";
-  //     catchPhrase: "Multi-layered client-server neural-net";
-  //     bs: "harness real-time e-markets";
-  //   };
 }
 
 export const userApi = createApi({
