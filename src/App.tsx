7b8f64435864539ebec2204cce7905d05f2d96fb
Johan Magnusson <magnusson1987@gmail.com>
2023-11-14 21:17:00 +0100

    add react router

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,60 @@ import './App.css';
 import { ReduxCounter } from './app/features/counterComponent';
 import { UserComponent } from './app/features/userComponent';
 import { UserTestComponent } from './app/features/userTestComponent';
+import { createBrowserRouter, Link, RouterProvider } from 'react-router-dom';
 
 function App() {
   const [count, setCount] = useState(0);
 
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: (
+        <>
+          <nav>
+            <ul>
+              <li>
+                <Link to={`reduxcounter`}>reduxcounter</Link>
+              </li>
+              <li>
+                <Link to={`localapi`}>localapi</Link>
+              </li>
+            </ul>
+          </nav>
+          <h1>React useState hook</h1>
+          <div className='card'>
+            <button onClick={() => setCount((count) => count + 1)}>
+              count is {count}
+            </button>
+          </div>
+        </>
+      ),
+
+      errorElement: <h1>Error. so sad =(</h1>,
+    },
+    {
+      path: '/reduxcounter',
+      element: <ReduxCounter />,
+      children: [
+        {
+          path: 'redux',
+          element: <h1>hej</h1>,
+        },
+      ],
+    },
+    {
+      path: '/localapi',
+      element: <UserTestComponent />,
+    },
+    {
+      path: '/externalapi',
+      element: <UserComponent />,
+    },
+  ]);
+
   return (
     <>
-      <h1>React useState hook</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <UserTestComponent />
-        <ReduxCounter />
-
-        <UserComponent />
-      </div>
+      <RouterProvider router={router} />
     </>
   );
 }
